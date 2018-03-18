8aa9038d45326f9c639235bf68bb5c425247135f
Dan Moore <github@mooreds.com>
2018-03-18 20:35:20 +0000

    first pass at having a graceful shutdown

--- main_test.go
+++ main_test.go
@@ -4,9 +4,9 @@ import "testing"
 import "time"
 
 
-func TestHash(t *testing.T) {
+func TestHashPassword(t *testing.T) {
     expected_hash := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
-    hash := hash("angryMonkey")
+    hash := hashPassword("angryMonkey")
     if hash != expected_hash {
        t.Errorf("Hash was incorrect, got: %s, want: %s.", hash, expected_hash)
     }
@@ -16,6 +16,15 @@ func TestSleepTimeSeconds(t *testing.T) {
 
     expected_val := time.Duration(5)
     val := sleepTimeSeconds()
+    if val != expected_val {
+       t.Errorf("Val was incorrect, got: %s, want: %s.", val, expected_val)
+    }
+}
+
+func TestNewShutdownValue(t *testing.T) {
+
+    expected_val := true
+    val := newShutdownValue()
     if val != expected_val {
        t.Errorf("Val was incorrect, got: %s, want: %s.", val, expected_val)
     }
