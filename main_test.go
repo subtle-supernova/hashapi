bca9900c74e581e6ab234aec021da6060ef0a297
Dan Moore <github@mooreds.com>
2018-03-18 22:03:55 +0000

    renaming of expected val, added first http test

--- main_test.go
+++ main_test.go
@@ -2,31 +2,52 @@ package main
 
 import "testing"
 import "time"
+import "net/http"
+import "net/http/httptest"
 
 
 func TestHashPassword(t *testing.T) {
-    expected_hash := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
+    expectedHash := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
     hash := hashPassword("angryMonkey")
-    if hash != expected_hash {
-       t.Errorf("Hash was incorrect, got: %s, want: %s.", hash, expected_hash)
+    if hash != expectedHash {
+       t.Errorf("Hash was incorrect, got: %s, want: %s.", hash, expectedHash)
     }
 }
 
 func TestSleepTimeSeconds(t *testing.T) {
 
-    expected_val := time.Duration(5)
+    expectedVal := time.Duration(5)
     val := sleepTimeSeconds()
-    if val != expected_val {
-       t.Errorf("Val was incorrect, got: %s, want: %s.", val, expected_val)
+    if val != expectedVal {
+       t.Errorf("Val was incorrect, got: %s, want: %s.", val, expectedVal)
     }
 }
 
 func TestNewShutdownValue(t *testing.T) {
 
-    expected_val := true
+    expectedVal := true
     val := newShutdownValue()
-    if val != expected_val {
-       t.Errorf("Val was incorrect, got: %s, want: %s.", val, expected_val)
+    if val != expectedVal {
+       t.Errorf("Val was incorrect, got: %s, want: %s.", val, expectedVal)
+    }
+}
+
+
+func TestRegisterShutdown(t *testing.T) {
+    req, err := http.NewRequest("GET", "/shutdown", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(registerShutdown)
+
+    handler.ServeHTTP(rr, req)
+
+    expectedVal := http.StatusOK
+    if status := rr.Code; status != expectedVal {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, expectedVal)
     }
 }
 
