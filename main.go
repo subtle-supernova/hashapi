ca2f3b9bbc5b9b6b73170069714550d3b5b40c81
Dan Moore <github@mooreds.com>
2018-03-20 01:55:50 +0000

    starting to add stats object and tests

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import "os"
 import "sync/atomic"
 import "strconv"
 import "strings"
+import "encoding/json"
+import "sync"
+
 
 const TIME_TO_SLEEP = 5 //TODO change to 5
 const PASSWORD_PARAM_NAME = "password"
@@ -17,21 +20,32 @@ const PASSWORD_PARAM_NAME = "password"
 const HASH_ENDPOINT_NAME = "/hash"
 const HASH_WITH_SLASH_ENDPOINT_NAME = "/hash/"
 const SHUTDOWN_ENDPOINT_NAME = "/shutdown"
+const CLEAN_SHUTDOWN_CODE = 0
+const SHUTDOWN_WAIT_CHECK = 1
 
 type PasswordInfo struct {
 	Id int
 	PasswordHash string
 }
 
+type Statistics struct {
+	Total int
+	CumulativeTime int
+	mux sync.Mutex
+}
+
 var inShutdownMode bool = false
 var handlingAHashRequest bool = false
 var hashId int32 = 0
 var seenPasswords map[int]PasswordInfo
+var stats Statistics
 
+// TODO refactor, put some stuff into packages
 
 func main() {
 
 	seenPasswords = make(map[int]PasswordInfo)
+	stats = *new(Statistics)
 
 	http.HandleFunc(HASH_ENDPOINT_NAME, hash)
 	http.HandleFunc(HASH_WITH_SLASH_ENDPOINT_NAME, hash)
@@ -46,18 +60,46 @@ func resetVariablesToStartingValues() { // TODO hack
 	handlingAHashRequest = false
 	hashId = 0
 	seenPasswords = make(map[int]PasswordInfo)
+	stats = *new(Statistics)
 }
 
 func checkForShutdownAndExit() {
 	for {
-		time.Sleep(1 * time.Second)
+		time.Sleep(SHUTDOWN_WAIT_CHECK * time.Second)
 		if inShutdownMode {
 			if handlingAHashRequest {
 				time.Sleep(sleepTimeSeconds() * time.Second)
 			}
-			os.Exit(0)
+			os.Exit(CLEAN_SHUTDOWN_CODE)
 		}
 	}
+}
+
+func statisticsGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, stats.statsOutput())
+}
+
+func (s *Statistics) statsOutput() string {
+	total := s.Total
+	averageTime := 0
+	if (total != 0) {
+		averageTime = s.CumulativeTime / s.Total
+	}
+	statsMap := map[string]int{"total": total, "average": averageTime}
+	jsonMap, _ := json.Marshal(statsMap)
+	return string(jsonMap)
+}
+
+func (s *Statistics) incrementTotal() {
+	s.mux.Lock()
+	s.Total += 1
+	s.mux.Unlock()
+}
+
+func (s *Statistics) incrementCumulativeTime(timeInMilliseconds int) {
+	s.mux.Lock()
+	s.CumulativeTime += timeInMilliseconds
+	s.mux.Unlock()
 }
 
 func registerShutdown(w http.ResponseWriter, r *http.Request) {
@@ -77,7 +119,8 @@ func getIdPointerFromPath(path string) *int {
 
 
 func hash(w http.ResponseWriter, r *http.Request) {
-	handlingAHashRequest = true
+	handlingAHashRequest = true // TODO this is wrong, need to update to lock it down better
+
 
 	if inShutdownMode {
 		w.WriteHeader(http.StatusServiceUnavailable)
@@ -85,6 +128,9 @@ func hash(w http.ResponseWriter, r *http.Request) {
 	}
 
 	id := 0
+
+// TODO stats should include any hashes returnbed, but not 404 errors
+// Use this: https://tour.golang.org/concurrency/9
 
 	var idPtr = getIdPointerFromPath(r.URL.Path)
 	if (idPtr != (*int)(nil)) {
@@ -97,6 +143,7 @@ func hash(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		} else {
+
 			fmt.Fprintf(w, passwordStruct.PasswordHash)
 			return
 		}
@@ -104,13 +151,17 @@ func hash(w http.ResponseWriter, r *http.Request) {
 
 	// TODO for the same password value we give different ids.
 
-	atomic.AddInt32(&hashId, 1)
-	fmt.Fprintln(w, hashId)
-	w.(http.Flusher).Flush()
 
 	r.ParseForm()
 	password := r.FormValue(PASSWORD_PARAM_NAME)
-	// TODO what if we don't see password?
+	if (password == "") {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	atomic.AddInt32(&hashId, 1)
+	fmt.Fprintln(w, hashId)
+	w.(http.Flusher).Flush()
 
 	time.Sleep(sleepTimeSeconds() * time.Second)
 	hashedPassword := hashPassword(password)
--- main_test.go
+++ main_test.go
@@ -5,13 +5,73 @@ import "time"
 import "strings"
 import "net/http"
 import "net/http/httptest"
-
+import "encoding/json"
+import "log"
 
 func TestHashPassword(t *testing.T) {
 	expectedHash := "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
 	hash := hashPassword("angryMonkey")
 	if hash != expectedHash {
 		t.Errorf("Hash was incorrect, got: %s, want: %s.", hash, expectedHash)
+	}
+}
+
+func TestStatsOutputEmpty(t *testing.T) {
+	stat := *new(Statistics)
+
+	expected := `{"average":0,"total":0}`
+	if stat.statsOutput() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+		stat.statsOutput(), expected)
+	}
+
+}
+
+func TestStatsOutputOneValue(t *testing.T) {
+	stat := *new(Statistics)
+	stat.Total = 1
+	stat.CumulativeTime = 100
+
+	expected := `{"average":100,"total":1}`
+	if stat.statsOutput() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+		stat.statsOutput(), expected)
+	}
+}
+
+func TestStatsOutputTwoValues(t *testing.T) {
+	stat := *new(Statistics)
+	stat.Total = 2
+	stat.CumulativeTime = 300
+
+	expected := `{"average":150,"total":2}`
+	if stat.statsOutput() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+		stat.statsOutput(), expected)
+	}
+}
+
+func TestStatsIncrementTotal(t *testing.T) {
+	stat := *new(Statistics)
+	stat.incrementTotal()
+	stat.incrementTotal()
+
+	expected := 2
+	if stat.Total != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+		stat.statsOutput(), expected)
+	}
+}
+
+func TestStatsIncrementCumulativeTime(t *testing.T) {
+	stat := *new(Statistics)
+	stat.incrementCumulativeTime(100)
+	stat.incrementCumulativeTime(200)
+
+	expected := 300
+	if stat.CumulativeTime != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+		stat.statsOutput(), expected)
 	}
 }
 
@@ -89,6 +149,25 @@ func TestHash(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	expectedVal := http.StatusOK
+	if status := rr.Code; status != expectedVal {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, expectedVal)
+	}
+}
+
+func TestHashNoPassword(t *testing.T) {
+	resetVariablesToStartingValues()
+	req, err := http.NewRequest("POST", "/hash", strings.NewReader("pasword=angryMonkey"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(hash)
+
+	handler.ServeHTTP(rr, req)
+
+	expectedVal := http.StatusBadRequest
 	if status := rr.Code; status != expectedVal {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, expectedVal)
@@ -173,3 +252,81 @@ func TestHashWithRequestIdAlreadySet(t *testing.T) {
 	}
 
 }
+
+func TestStatisticsEmpty(t *testing.T) {
+	resetVariablesToStartingValues()
+	req, err := http.NewRequest("GET", "/stats", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(statisticsGet)
+
+	handler.ServeHTTP(rr, req)
+
+	expectedVal := http.StatusOK
+	if status := rr.Code; status != expectedVal {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, expectedVal)
+	}
+
+	expected := `{"average":0,"total":0}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+		rr.Body.String(), expected)
+	}
+
+}
+
+func TestStatisticsOneRequest(t *testing.T) {
+	resetVariablesToStartingValues()
+	initialreq, err := http.NewRequest("POST", "/hash", strings.NewReader("password=angryMonkey"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+
+	req, err := http.NewRequest("GET", "/stats", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	initialhandler := http.HandlerFunc(hash)
+
+	initialhandler.ServeHTTP(rr, initialreq)
+
+	rrForStats := httptest.NewRecorder()
+	handler := http.HandlerFunc(statisticsGet)
+
+	handler.ServeHTTP(rrForStats, req)
+
+	expectedVal := http.StatusOK
+	if status := rr.Code; status != expectedVal {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, expectedVal)
+	}
+
+	var dat map[string]int
+	bodyStr := rrForStats.Body.String()
+	body := []byte(bodyStr)
+
+	if err := json.Unmarshal(body, &dat); err != nil {
+		t.Errorf("handler returned unexpected json: %v", bodyStr)
+	}
+	log.Print(dat)
+
+	expectedTotal := 1
+	unexpectedAverage := 0
+	if dat["total"] != expectedTotal {
+		t.Errorf("handler returned unexpected total: got %v want %v",
+		dat["total"], expectedTotal)
+	}
+	if dat["average"] == unexpectedAverage {
+		t.Errorf("handler returned unexpected avreage: got %v don't want %v",
+		dat["average"], unexpectedAverage)
+	}
+
+}
+
